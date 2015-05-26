a658bd0764809844e6308554f629156ad9408e9f
AndersonBrantes <andersonbrantes@gmail.com>
2015-05-25 22:59:42 -0300

    implementando metodo para verificar se livro possui reimpressao

--- livro.rb
+++ livro.rb
@@ -2,17 +2,23 @@ class Livro
 	
 	attr_reader :titulo, :preco, :ano_lancamento
 
-	def initialize(titulo, preco, ano_lancamento)
+	def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
 
 		@titulo = titulo
 		@ano_lancamento = ano_lancamento
+		@possui_reimpressao = possui_reimpressao
 		@preco = calcula_preco(preco)
 	end
 
 	def calcula_preco(base)
-
-		if @ano_lancamento < 2000
-			base *= 0.7
+		if @ano_lancamento < 2006
+			if @possui_reimpressao
+				base * 0.9
+			else
+				base * 0.95
+			end
+		elsif @ano_lancamento <= 2010
+			base * 0.96
 		else
 			base
 		end
@@ -29,5 +35,5 @@ def livro_para_newsletter(livro)
 end
 
 
-algoritmos = Livro.new("Algoritmos", 100, 1998)
+algoritmos = Livro.new("Algoritmos", 100, 1998, true)
 livro_para_newsletter(algoritmos)
